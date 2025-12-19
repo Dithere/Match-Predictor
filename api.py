458f6046f19a50f4a5a0c87060f77da7aacecca6
Aditya Narayan Mishra <dithere10@gmail.com>
2025-12-20 01:21:12 +0530

    Update api.py

--- api.py
+++ api.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 import pandas as pd
 import joblib
 from flask_cors import CORS
-# Load model + encoders
+
 bundle = joblib.load("match_prediction.joblib")
 model = bundle["model"]
 encoders = bundle["encoders"]
@@ -13,8 +13,7 @@ CORS(app)
 def predict():
     data = request.get_json()
 
-    try:
-        # Prepare input for prediction
+    try:
         input_data = pd.DataFrame([{
             "team1": encoders["team1"].transform([data["team1"]])[0],
             "team2": encoders["team2"].transform([data["team2"]])[0],
@@ -22,8 +21,7 @@ def predict():
             "toss_winner": encoders["toss_winner"].transform([data["toss_winner"]])[0],
             "toss_decision": encoders["toss_decision"].transform([data["toss_decision"]])[0]
         }])
-
-        # Predict
+
         winner_encoded = model.predict(input_data)[0]
         predicted_winner = encoders["winner"].inverse_transform([winner_encoded])[0]
 
